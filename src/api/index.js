7a32ccfdfb1a3f93b51c83c568e91ac6bc5d1ef1
Andres Noboa V <aenoboa1@espe.edu.ec>
2023-07-18 19:32:33 -0500

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+export const BASE_URL = 'https://64ac56039edb4181202f712a.mockapi.io/';
+export const BASE_URL_DEPOSIT = 'https://64b6cddfdf0839c97e1625e9.mockapi.io/deposits/';
+
+export const ENDPOINTS = {
+    accounts: 'accounts',
+}
+
+export const ENDPOINTSDEPOSIT = {
+    deposit: 'deposit',
+}
+
+export const createAPIEndpoint = endpoint => {
+
+    let url = BASE_URL + endpoint + '/';
+    return {
+        fetch: (token) => axios.get(url, token),
+        post: (newRecord, token) => axios.post(url, newRecord, token),
+        put: (id, updatedRecord, token) => axios.put(url + id, updatedRecord, token),
+        delete: id => axios.delete(url + id),
+        fetchById: (id, token) => axios.get(url + id, token),
+    }
+}
+
+export const createAPIEndpointDeposit = endpoint => {
+
+    let url = BASE_URL_DEPOSIT + endpoint + '/';
+    return {
+        fetch: (token) => axios.get(url, token),
+        post: (newRecord, token) => axios.post(url, newRecord, token),
+        put: (id, updatedRecord, token) => axios.put(url + id, updatedRecord, token),
+        delete: id => axios.delete(url + id),
+        fetchById: (id, token) => axios.get(url + id, token),
+    }
+}
---
+++ src/pages/solicitud-prestamos/index.js
@@ -0,0 +1,39 @@
+/**
+=========================================================
+* Soft UI Dashboard React - v4.0.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Grid from "@mui/material/Grid";
+
+// Soft UI Dashboard React components
+import SoftBox from "components/SoftBox";
+
+
+// Soft UI Dashboard React examples
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import BasicCard from "./components/BasicCard";
+
+
+function Prestamos() {
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+        <BasicCard/>
+
+    </DashboardLayout>
+  );
+}
+
+export default Prestamos;
